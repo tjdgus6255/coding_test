f965b2adff41294047382589f4bfbf43b7c95926
tjdgus6255 <tjdgus6255@naver.com>
2021-08-04 21:49:59 +0900

    2439_bj

---
+++ 1110_bj.py
@@ -0,0 +1,13 @@
+InputN = int(input())
+count = 0
+N = InputN
+Ncycle = 0
+
+while True:
+  Ncycle = ((N % 10) * 10) + ((N // 10) + (N % 10))%10
+  count += 1
+  if InputN == Ncycle:
+    break
+  N = Ncycle
+
+print(count)
